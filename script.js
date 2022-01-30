e5dec968f546991c2cf24c1731c11b089f12c4ff
Nikolay Lamanov <59505751+hard-it@users.noreply.github.com>
2022-01-30 23:29:46 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,2 @@
+/* Copy this code in console on the group page */
+(function () { 'use strict'; if (!confirm('Удалить все записи со стены?')) return; var deletePostLink = document.body.querySelectorAll('a.ui_actions_menu_item[onclick^="wall.deletePost"]');for (var i = 0; i < deletePostLink.length; i++) { deletePostLink[i].click(); } alert(deletePostLink.length + ' posts deleted'); }());
